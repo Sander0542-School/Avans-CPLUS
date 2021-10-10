6c45f435336f65e5fd11bf9fa2116fa90357cf1b
Sander Jochems <sanderjochems@hotmail.nl>
2021-10-10 16:55:03 +0200

    Update HttpClient.cpp

--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -17,7 +17,7 @@ size_t CurlWrite_CallbackFunc_StdString(char* contents, size_t size, size_t nmem
 bool HttpClient::get(CURL* curl, const std::string& url, std::string* contents) {
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &CurlWrite_CallbackFunc_StdString);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, contents);
     curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
